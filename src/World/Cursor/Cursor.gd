d613b13d732ff6dcb2c9f6b8b837613bbeddfc0f
Your Name <silver1miner@gmail.com>
2022-01-01 10:56:04 -0800

    world, cursor, camera

---
+++ src/World/Cursor/Cursor.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+export var Grid: Resource = preload("res://src/World/Grid.tres")
+export var ui_cooldown := 0.1
+onready var _timer: Timer = $Timer
+onready var _camera: Camera2D = $Camera2D
+
+export var cell := Vector2.ZERO setget set_cell
+signal cursor_moved(cell)
+signal accept_pressed(cell)
+signal cancel_pressed(cell)
+
+func _ready() -> void:
+	_timer.wait_time = ui_cooldown
+	set_cell(Grid.get_cell_coordinates(position))
+	position = Grid.get_map_position(cell)
+
+func bound_camera() -> void:
+	_camera.limit_left = 0
+	_camera.limit_top = 0
+	_camera.limit_right = Grid.get_map_bounds().x * Grid.cell_size.x
+	_camera.limit_bottom = Grid.get_map_bounds().y * Grid.cell_size.y
+
+func set_cell(input: Vector2) -> void:
+	var new_cell: Vector2 = Grid.clamp_to_board(input)
+	if new_cell.is_equal_approx(cell):
+		return
+	cell = new_cell
+	position = Grid.get_map_position(cell)
+
+var past_cell := cell
+func _unhandled_input(event) -> void:
+	if event is InputEventMouseMotion:
+		if Grid.is_within_bounds(Grid.get_cell_coordinates(get_global_mouse_position())):
+			visible = true
+			self.cell = Grid.get_cell_coordinates(get_global_mouse_position())
+			if cell != past_cell:
+				emit_signal("cursor_moved", cell)
+			past_cell = cell
+		else:
+			visible = false
+	if event.is_action_pressed("ui_accept") or event.is_action_pressed("click_left"):
+		emit_signal("accept_pressed", cell)
+		get_tree().set_input_as_handled()
+	if event.is_action_pressed("ui_cancel") or event.is_action_pressed("click_right"):
+		emit_signal("cancel_pressed", cell)
+		get_tree().set_input_as_handled()
+	var should_move = event.is_pressed()
+	if event.is_echo():
+		should_move = should_move and _timer.is_stopped()
+	if not should_move:
+		return
+	if event.is_action("ui_right"):
+		self.cell += Vector2.RIGHT
+		emit_signal("cursor_moved", cell)
+	elif event.is_action("ui_left"):
+		self.cell += Vector2.LEFT
+		emit_signal("cursor_moved", cell)
+	if event.is_action("ui_up"):
+		self.cell += Vector2.UP
+		emit_signal("cursor_moved", cell)
+	elif event.is_action("ui_down"):
+		self.cell += Vector2.DOWN
+		emit_signal("cursor_moved", cell)
---
+++ src/World/World.gd
@@ -0,0 +1,23 @@
+extends Node
+
+export var map_size = Vector2(25, 18)
+export var Grid: Resource = preload("res://src/World/Grid.tres")
+
+func _ready() -> void:
+	Grid.board_size = map_size
+	$Cursor.bound_camera()
+	if $Cursor.connect("cursor_moved", self, "_on_cursor_moved") != OK:
+		push_error("connect fail")
+	if $Cursor.connect("accept_pressed", self, "_on_accept_pressed") != OK:
+		push_error("connect fail")
+	if $Cursor.connect("cancel_pressed", self, "_on_cancel_pressed") != OK:
+		push_error("connect fail")
+
+func _on_cursor_moved(cell) -> void:
+	print("cursor moved to ", cell)
+
+func _on_accept_pressed(cell) -> void:
+	print("pressed accept at ", cell)
+
+func _on_cancel_pressed(cell) -> void:
+	print("pressed cancel at ", cell)
