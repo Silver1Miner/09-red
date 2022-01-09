008add96ab0604ab6eb3c3ebda8c8f368f9adf30
Your Name <silver1miner@gmail.com>
2022-01-09 13:54:30 -0800

    main menu, settings menu

---
+++ src/Menu/MainMenu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+onready var quit_button = $Options/Quit
+onready var settings_menu = $Settings
+
+func _ready() -> void:
+	if OS.get_name() == "HTML5":
+		quit_button.visible = false
+
+func _on_NewGame_pressed() -> void:
+	print("New Game")
+	if get_tree().change_scene("res://src/world/World.tscn") != OK:
+		push_error("fail to load world")
+
+func _on_LoadGame_pressed() -> void:
+	print("Load Game")
+	if get_tree().change_scene("res://src/world/World.tscn") != OK:
+		push_error("fail to load world")
+
+func _on_Settings_pressed() -> void:
+	settings_menu.visible = true
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
---
+++ src/Player/PlayerData.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var music_db = 0.5
+var sound_db = 0.1
+
+func load_player_data() -> void:
+	pass
+
+func save_player_data() -> void:
+	pass
